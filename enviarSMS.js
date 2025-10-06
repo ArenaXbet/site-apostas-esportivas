45483c80a5485ce4b78d346cad2c09f805317a07
ArenaXbet <jecksonbarbosa161@gmail.com>
2025-10-06 20:36:28 -0300

    Update enviarSMS.js

--- enviarSMS.js
+++ enviarSMS.js
@@ -1,19 +1,18 @@
-enviarSMS.js
 import twilio from 'twilio';
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
-export async function enviarSMS(to, mensagem) {
+export async function enviarSMS(para, mensagem) {
   try {
     const sms = await client.messages.create({
       body: mensagem,
-      from: '+15312245450', // <-- seu número Twilio
-      to,
+      from: process.env.TWILIO_PHONE_NUMBER, // número Twilio puxado das variáveis
+      to: para, // número de destino (ex: +5511988805285)
     });
-    console.log('SMS enviado com sucesso:', sms.sid);
+    console.log('✅ SMS enviado com sucesso:', sms.sid);
   } catch (erro) {
-    console.error('Erro ao enviar SMS:', erro);
+    console.error('❌ Erro ao enviar SMS:', erro);
   }
-  }
+}
